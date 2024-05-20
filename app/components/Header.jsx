7fb4ac1e44216cdf1c6466ae5f01b3b15ffd0f24
obrienwatersports <nsteinbauer@obrien.com>
2024-05-20 13:41:42 -0400

    new header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -23,7 +23,8 @@ export function Header({header, isLoggedIn, cart}) {
       <div role="banner" className={`always-flex justify topHeader`}>
         <div className="flex-vertical promo-banner">
           <span>
-            Direct Web Sales Available for US Residents in the lower 48 States
+            {/* Direct Web Sales Available for US Residents in the lower 48 States */}
+            Now through Memorial Day, Free shipping on all products site wide.
           </span>
         </div>
         <CartToggle cart={cart} />
